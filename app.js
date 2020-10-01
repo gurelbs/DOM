74e78db42f16a488b38f4694a3db116f22a7e075
גוראל בן שבת <jobisraeliim@gmail.com>
2020-10-01 19:41:13 +0300

    all

--- app.js
+++ app.js
@@ -6,14 +6,39 @@ function defaultFontSize(){
 }
 function changeFontSize(x, num){
     txt =  document.querySelector(x);
-    style = window.getComputedStyle(txt, null).getPropertyValue('font-size');
-    currentSize = parseFloat(style);
+    fSize = window.getComputedStyle(txt).getPropertyValue('font-size');
+    currentSize = parseFloat(fSize);
 
     if (currentSize < maxSize && currentSize > minSize) {
         txt.style.fontSize = (currentSize + num) + 'px';
         if (currentSize < maxSize){
-            para = document.querySelector('.para').innerHTML=`הגודל הנוכחי עומד על ${parseFloat(style) + num}px`;
+            para = document.querySelector('.para').innerHTML=`הגודל הנוכחי עומד על ${parseFloat(fSize) + num}px`;
         } 
     }
 }
 
+let header = document.querySelector('#h1');
+let increase = document.querySelector('.plus');
+let decrease = document.querySelector('.minus');
+
+function increaseBtn(){
+    fSize = window.getComputedStyle(header).getPropertyValue('font-size');
+    let pxNum = parseFloat(fSize)
+    increase.addEventListener('click', () => {
+        if (pxNum > minSize && pxNum < maxSize){
+            header.style.fontSize = `${pxNum + 2}px`;
+        }
+        console.log(fSize);
+    });
+}
+
+function decreaseBtn(){
+    fSize = window.getComputedStyle(header).getPropertyValue('font-size');
+    let pxNum = parseFloat(fSize)
+    decrease.addEventListener('click', () => {
+        if (pxNum > minSize && pxNum < maxSize){
+            header.style.fontSize = `${pxNum - 2}px`;
+        }
+        console.log(fSize);
+    });
+}
